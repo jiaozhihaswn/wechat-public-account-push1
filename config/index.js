b59c394178c3f524ed9ebeca418f8c0fd4eeefab
jiaozhihaswn <113079190+jiaozhihaswn@users.noreply.github.com>
2022-09-08 15:05:01 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -16,9 +16,9 @@ export const config = {
    *    // 想要发送的人的名字
    *    name: "小王同志","小李同志",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "okFIj56WHKoXf1tbNjjdY3I6rEt0","okFIj55kB8y_5Su4aLzpl6vU52Q0",
+   *    id: "",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "4m6mJtOAPt6Q9ZdxhznUyr8Kd0ShwGJtOdfptCkvGz4",
+   *    useTemplateId: "wro4ztVBHNVWQ_PoQQQR8M2ruCLoDGoSwYCFCah27gQ",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
